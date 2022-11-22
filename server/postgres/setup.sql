488946dfee7c5dbb7f8155b65384778ab6c813e4
Katarina Slotova <kslotova@c2r2p11.hive.fi>
2022-11-22 17:27:03 +0200

    Modified db structure.

--- server/postgres/setup.sql
+++ server/postgres/setup.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS users (
 	image TEXT DEFAULT 'none',
 	age INT(11) NOT NULL,
 	gender VARCHAR(1000) NOT NULL,
-	sexual_orient VARCHAR(1000) NOT NULL,
+	sexual_orient VARCHAR(1000) DEFAULT 'bisexual',
 	bio VARCHAR(1000),
 	city VARCHAR(1000),
 	country VARCHAR(1000),
@@ -16,14 +16,15 @@ CREATE TABLE IF NOT EXISTS users (
 	email VARCHAR(1000), 
 	active TINYINT(1) DEFAULT 0,
 	token varchar(255) NOT NULL,
-	popularity INT(11) NOT NULL DEFAULT 0, 
+	fame INT(11) NOT NULL DEFAULT 0,
+	online INYINT(1) DEFAULT 0,
 	last_seen TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS user_images (
 	id BIGSERIAL NOT NULL PRIMARY KEY,
 	user_id INT(11) NOT NULL, 
-	imgage TEXT NOT NULL
+	image TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS matches (
